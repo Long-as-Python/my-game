46151d298fa7d170a952f4f34224b49d1ae8c272
Danylo Lahodniuk <45416328+Long-as-Python@users.noreply.github.com>
2019-10-09 19:52:06 +0300

    fix2

---
+++ Hello World my first game/Assets/Scripts/herowalking.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class herowalking : MonoBehaviour
+
+{
+    public float speed = 10f;
+    public float jumpPower = 500f;
+    Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move;
+        move = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(move * speed, rig.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(new Vector2(0, jumpPower));
+        }
+    }
+}
