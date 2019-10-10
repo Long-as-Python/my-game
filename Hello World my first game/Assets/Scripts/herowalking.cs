c82417a69f283178f33399ec05f12194591f380d
Long-as-Python <45416328+Long-as-Python@users.noreply.github.com>
2019-10-10 14:43:56 +0300

    improovements and fixes
    first build coins boxes

--- Hello World my first game/Assets/Scripts/herowalking.cs
+++ Hello World my first game/Assets/Scripts/herowalking.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class herowalking : MonoBehaviour
 
 {
+    bool facingRight = true;
+    bool grounded = false;
+    public Transform groundcheck;
+    public float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+
     public float speed = 10f;
     public float jumpPower = 500f;
     Rigidbody2D rig;
@@ -18,13 +25,36 @@ public class herowalking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        grounded = Physics2D.OverlapCircle(groundcheck.position, groundRadius, whatIsGround);
+
         float move;
         move = Input.GetAxis("Horizontal");
         rig.velocity = new Vector2(move * speed, rig.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             rig.AddForce(new Vector2(0, jumpPower));
+        }
+
+        if ((move < 0) && facingRight)
+            Flip();
+        else if ((move > 0) && !facingRight)
+            Flip();
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        if (col.GetComponent<CircleCollider2D>().tag == "coin")
+        {
+            Destroy(col.gameObject);
         }
     }
 }
